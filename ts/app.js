4665a0d7026b484243896db615813a6fe934a027
rubenhierro <rubenhierro@gmail.com>
2022-03-03 00:55:33 +0100

    API Weather

--- ts/app.js
+++ ts/app.js
@@ -1,15 +1,135 @@
-const loadJokes = async () => {
-    try {
-        const loadJokes = await fetch("https://icanhazdadjoke.com/", {
-            headers: {
-                Accept: "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    }
-    catch (error) {
-        console.log(error);
+// --- API Calls --- //
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-loadJokes();
+var _this = this;
+var loadJokes = function () { return __awaiter(_this, void 0, void 0, function () {
+    var loadJokes_1, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, fetch("https://icanhazdadjoke.com/", {
+                        headers: {
+                            Accept: "application/json"
+                        }
+                    })
+                        .then(function (response) { return response.json(); })
+                        .then(function (data) { return printJoke(data); })];
+            case 1:
+                loadJokes_1 = _a.sent();
+                return [3 /*break*/, 3];
+            case 2:
+                error_1 = _a.sent();
+                console.log(error_1);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
+var loadChuckJokes = function () { return __awaiter(_this, void 0, void 0, function () {
+    var loadChuckJokes_1, error_2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, fetch("https://api.chucknorris.io/jokes/random")
+                        .then(function (response) { return response.json(); })
+                        .then(function (data) { return printChuckJoke(data); })];
+            case 1:
+                loadChuckJokes_1 = _a.sent();
+                return [3 /*break*/, 3];
+            case 2:
+                error_2 = _a.sent();
+                console.log(error_2);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
+var loadWeather = function () { return __awaiter(_this, void 0, void 0, function () {
+    var loadWeather_1, error_3;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, fetch('https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=79249a6da5a4a52e43bdc6a5bce6de66')
+                        .then(function (response) { return response.json(); })
+                        .then(function (data) { return printWeather(data); })];
+            case 1:
+                loadWeather_1 = _a.sent();
+                return [3 /*break*/, 3];
+            case 2:
+                error_3 = _a.sent();
+                console.log(error_3);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
+// --- End API Calls --- //
+var nextJoke = document.getElementById('button');
+var joke = document.getElementById('joke');
+var iconWeather = document.getElementById('icon-weather');
+var textWeather = document.getElementById('text-weather');
+var randomJoke = function () {
+    var flipCoin = Math.random() * 10;
+    return flipCoin < 5 ? loadJokes() : loadChuckJokes();
+};
+// --- Events --- //
+window.addEventListener('load', function () {
+    loadWeather();
+});
+nextJoke.addEventListener('click', function () {
+    randomJoke();
+    printJoke(joke);
+});
+var printJoke = function (text) {
+    if (text.joke !== undefined) {
+        joke.innerHTML = text.joke;
+    }
+};
+var printChuckJoke = function (text) {
+    if (text.value !== undefined) {
+        joke.innerHTML = text.value;
+    }
+};
+function printWeather(_a) {
+    var weather = _a.weather;
+    var w = weather[0];
+    //falta imatge
+    textWeather.innerHTML = w.main;
+}
--- ts/app.ts
+++ ts/app.ts
@@ -1,3 +1,5 @@
+// --- API Calls --- //
+
 const loadJokes = async () => {
   try {
     const loadJokes = await fetch("https://icanhazdadjoke.com/", {
@@ -6,10 +8,70 @@ const loadJokes = async () => {
       },
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => printJoke(data));
   } catch (error) {
     console.log(error);
   }
 };
 
-loadJokes();
+const loadChuckJokes = async () => {
+  try {
+    const loadChuckJokes = await fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((data) => printChuckJoke(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const loadWeather = async () => {
+  try {
+    const loadWeather = await fetch('https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=79249a6da5a4a52e43bdc6a5bce6de66')
+      .then((response) => response.json())
+      .then((data) => printWeather(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+// --- End API Calls --- //
+
+
+const nextJoke = document.getElementById('button');
+const joke = document.getElementById('joke');
+const iconWeather = document.getElementById('icon-weather');
+const textWeather = document.getElementById('text-weather');
+
+
+const randomJoke = () => {
+  const flipCoin = Math.random() * 10;
+  return flipCoin < 5 ? loadJokes() : loadChuckJokes();
+}
+
+// --- Events --- //
+window.addEventListener('load', () => {
+  loadWeather()
+});
+
+nextJoke.addEventListener('click', () => {
+  randomJoke();
+  printJoke(joke);
+});
+
+const printJoke = (text) => {
+  if (text.joke !== undefined) {
+    joke.innerHTML = text.joke;
+  }
+};
+
+const printChuckJoke = (text) => {
+  if (text.value !== undefined) {
+    joke.innerHTML = text.value;
+  }
+}
+
+function printWeather ({weather}) {
+    const [w] = weather;
+    //falta imatge
+    textWeather.innerHTML = w.main;
+}
+
