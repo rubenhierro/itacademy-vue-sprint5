8ccb0dc3fd6dbdd3e68e0f02bd0f9530f13775a9
rubenhierro <rubenhierro@gmail.com>
2022-03-02 18:42:45 +0100

    fetch function

---
+++ ts/app.js
@@ -0,0 +1,15 @@
+const loadJokes = async () => {
+    try {
+        const loadJokes = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+loadJokes();
